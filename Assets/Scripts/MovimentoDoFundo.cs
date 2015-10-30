b6895b961ef7e8c76f60f9941bcbde65e7e1b547
XxJPSxX <jp.medeiros_11@yahoo.com.br>
2015-10-30 01:04:03 -0200

    0.04
    Inicio do HUD

--- Assets/Scripts/MovimentoDoFundo.cs
+++ Assets/Scripts/MovimentoDoFundo.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MovimentoDoFundo : MonoBehaviour {
 	public float velocidade = 0.1f;
 	public bool ativador = true;
+	public float posYFinal = -20.79f;
+	public float posYInicial = 20.81f;
+	public float posZ = 2;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +20,15 @@ public class MovimentoDoFundo : MonoBehaviour {
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -velocidade));
 		}
 
+<<<<<<< HEAD
 		if (transform.position.y < -10.79f){
 
 			transform.position = new Vector3(transform.position.x,10.81f,2);
+=======
+		if (transform.position.y < posYFinal){
+
+			transform.position = new Vector3(transform.position.x,posYInicial,posZ);
+>>>>>>> refs/remotes/origin/master
 		}
 		//float posX = transform.position.x;
 		//float posY = transform.position.y;
