eda301494c0e3b9a53d060a05c54ada5aeb35665
XxJPSxX <jp.medeiros_11@yahoo.com.br>
2015-10-30 13:09:20 -0200

    0.042
    + HUD

---
+++ Assets/Scripts/Transparencia.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transparencia : MonoBehaviour {
+	//Este script controla a transparencia das sprites(criado para o HUD)
+	// transparencia, 0 = 100% transparente , 0.3 = 70% transparente, 0.5 = 50% transparente, 1 = 0% transparente
+	public float transparencia = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer renderizador =  GetComponent<SpriteRenderer>();
+		renderizador.color = new Color(1f,1f,1f,transparencia);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
